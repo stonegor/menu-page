95d199b18675a55f2384c2068a735a0bb77448b8
geoheat.ed_vkusvill.ru <sstonegor@gmail.com>
2025-10-20 12:51:33 +0300

    feat: yandex metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import type React from "react"
 import type { Metadata } from "next"
 import localFont from "next/font/local"
@@ -52,7 +53,25 @@ export default function RootLayout({
         className={`font-sans ${villula.variable} ${euclidRegular.variable} ${euclidBold.variable} ${euclidSemibold.variable}`}
       >
         <Suspense fallback={null}>
-          <Header />
+          <Script
+            id="yandex-metrika"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                ym(103116177, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+            }}
+          />
         </Suspense>
         {children}
       </body>
